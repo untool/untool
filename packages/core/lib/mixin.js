29328465066416f09fd93ca92283f457a860c462
dmbch <daniel@dmbch.net>
2018-04-19 22:08:44 +0200

    chore(core): update to mixinable v2.0

--- packages/core/lib/mixin.js
+++ packages/core/lib/mixin.js
@@ -1,10 +1,7 @@
-const { bindAll } = require('./util');
-
 module.exports = class Mixin {
   constructor(core, config) {
     this.core = core;
     this.config = config;
     this.options = {};
-    bindAll(this);
   }
 };
