ae5dbe0d8a73f09ee2539fdaccdae7ed1e769b65
dmbch <daniel@dmbch.net>
2018-04-24 00:05:22 +0200

    refactor(react): reorganize and tweak template

--- packages/react/lib/template.js
+++ packages/react/lib/template.js
@@ -1,5 +1,9 @@
 const esc = require('serialize-javascript');
 
+const variable = ({ name, value }) => `<script>${name}=${esc(value)}</script>`;
+const cssLink = css => `<link rel="stylesheet" href="/${css}" />`;
+const jsLink = js => `<script src="/${js}"></script>`;
+
 module.exports = data =>
   `<!DOCTYPE html>
 <html ${data.helmet.htmlAttributes.toString()}>
@@ -8,20 +12,14 @@ module.exports = data =>
     ${data.helmet.base.toString()}
     ${data.helmet.meta.toString()}
     ${data.helmet.link.toString()}
-    ${data.assetsByType.css
-      .map(css => `<link rel="stylesheet" href="/${css}" />`)
-      .join('')}
+    ${data.assetsByType.css.map(cssLink).join('')}
     ${data.helmet.style.toString()}
     ${data.helmet.script.toString()}
   </head>
   <body ${data.helmet.bodyAttributes.toString()}>
     <div id="${data.mountpoint}">${data.markup}</div>
     ${data.helmet.noscript.toString()}
-    ${data.globals
-      .map(global => `<script>${global.name}=${esc(global.value)}</script>`)
-      .join('')}
-    ${data.assetsByType.js
-      .map(js => `<script src="/${js}" async></script>`)
-      .join('')}
+    ${data.globals.map(variable).join('')}
+    ${data.assetsByType.js.map(jsLink).join('')}
   </body>
 </html>`.replace(/(^\s*[\r\n]| (?=>))/gm, '');
