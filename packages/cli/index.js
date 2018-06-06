2330b8027d23a75ab997d5eb80b5b75ca98b14f2
dmbch <daniel@dmbch.net>
2018-06-07 00:12:39 +0200

    refactor(cli): improve error handling

--- packages/cli/index.js
+++ packages/cli/index.js
@@ -1,18 +1,17 @@
 #!/usr/bin/env node
 'use strict';
 
-const { promisify } = require('util');
 const { dirname } = require('path');
 
-const findUp = require('find-up');
-const resolve = promisify(require('enhanced-resolve'));
+const { sync: findUp } = require('find-up');
+const { sync: resolve } = require('enhanced-resolve');
 
-findUp('package.json')
-  .then(pkgFile =>
-    resolve(dirname(pkgFile), '@untool/yargs').then(path => require(path).run())
-  )
-  .catch(error => {
-    // eslint-disable-next-line no-console
-    console.error(error.stack ? error.stack.toString() : error.toString());
-    process.exit(1);
-  });
+try {
+  const pkgFile = findUp('package.json');
+  const yargsPath = resolve(dirname(pkgFile), '@untool/yargs');
+  require(yargsPath).run();
+} catch (_) {
+  // eslint-disable-next-line no-console
+  console.error("Error: Can't find @untool/yargs \n");
+  process.exit(1);
+}
