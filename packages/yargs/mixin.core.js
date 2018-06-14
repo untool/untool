272460be5dbb1000b3f7e195963b816bf4b30685
dmbch <daniel@dmbch.net>
2018-06-14 21:40:11 +0200

    refactor(yargs): unopinionate: no more logging hooks

--- packages/yargs/mixin.core.js
+++ packages/yargs/mixin.core.js
@@ -1,6 +1,3 @@
-/* eslint-disable no-console */
-const chalk = require('chalk');
-
 const {
   sync: { pipe, sequence, override },
 } = require('mixinable');
@@ -8,59 +5,10 @@ const {
 const { Mixin } = require('@untool/core');
 
 class YargsMixin extends Mixin {
-  registerCommands(yargs) {
-    return yargs.option('log', {
-      alias: 'l',
-      default: 'info',
-      describe: 'Define log level',
-      type: 'string',
-    });
-  }
-  handleArguments(argv) {
-    const levels = ['debug', 'info', 'warn', 'error', 'silent'];
-    const index = levels.indexOf(argv.log);
-    this.levels = levels.slice(Math.max(index, 0), -1);
-  }
   handleError(error) {
-    this.logError(error);
+    // eslint-disable-next-line no-console
+    console.error(error.stack ? error.stack.toString() : error.toString());
     process.exit(1);
-  }
-  logDebug(...args) {
-    const { name } = this.config;
-    if (this.levels && this.levels.includes('debug')) {
-      console.log(chalk.bold(`${name} debug`));
-      console.debug(...args);
-    }
-  }
-  logInfo(...args) {
-    const { name } = this.config;
-    if (this.levels && this.levels.includes('info')) {
-      console.log(chalk.bold(`${name} info`));
-      console.info(...args);
-    }
-  }
-  logWarn(...args) {
-    const { name } = this.config;
-    if (this.levels && this.levels.includes('warn')) {
-      console.log(chalk.yellow.bold(`${name} warn`));
-      console.warn(...args);
-    }
-  }
-  logError(error) {
-    const { name } = this.config;
-    if (!this.levels || this.levels.includes('error')) {
-      console.log(chalk.red.bold(`${name} error`));
-      console.error(error.stack ? error.stack.toString() : error.toString());
-    }
-  }
-  logStats(stats) {
-    const { name } = this.config;
-    if (this.levels && this.levels.includes('info')) {
-      console.log(chalk.bold(`${name} stats`));
-      console.info(
-        stats.toString({ chunks: false, modules: false, entrypoints: false })
-      );
-    }
   }
 }
 
@@ -68,11 +16,6 @@ YargsMixin.strategies = {
   registerCommands: pipe,
   handleArguments: sequence,
   handleError: override,
-  logDebug: override,
-  logInfo: override,
-  logWarn: override,
-  logError: override,
-  logStats: override,
 };
 
 module.exports = YargsMixin;
