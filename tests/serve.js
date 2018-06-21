7973a477820fca4f8810d37dc99d3f26f1b53f9a
dmbch <daniel@dmbch.net>
2018-06-21 17:40:33 +0200

    test: update tests for `configureCommand` hook

--- tests/serve.js
+++ tests/serve.js
@@ -3,7 +3,7 @@ const test = require('ava');
 const prepare = require('./helpers/prepare');
 
 test('core lifecycle hooks', (t) =>
-  prepare('serve', '-s').then((api) =>
+  prepare('serve').then((api) =>
     Promise.all([
       api.getArgTypes('constructor').then((args) => t.snapshot(args)),
       api.getArgTypes('registerCommands').then((args) => t.snapshot(args)),
