d4d57e2a46e612d3ce9f4a8b83cc5337bd42b9ca
dmbch <daniel@dmbch.net>
2018-06-29 01:19:47 +0200

    feat(webpack): add env specific config placeholders

--- packages/webpack/lib/shims/node.js
+++ packages/webpack/lib/shims/node.js
@@ -2,11 +2,13 @@
 
 require('babel-polyfill');
 
-var { getConfig } = require('@untool/config');
-var entryPoint = require('@untool/entrypoint');
+const { getConfigAndMixins } = require('@untool/config');
 
+let entryPoint = require('@untool/entrypoint');
 if (typeof entryPoint.default === 'function') {
   entryPoint = entryPoint.default;
 }
 
-module.exports = entryPoint(getConfig());
+const { config, mixins } = getConfigAndMixins();
+
+module.exports = entryPoint(config, mixins);
