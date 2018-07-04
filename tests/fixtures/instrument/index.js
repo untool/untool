f41b3d80d7150e2e8724b00df141e8a8958520dc
dmbch <daniel@dmbch.net>
2018-07-05 00:12:25 +0200

    test: require `@untool/core` directly

--- tests/fixtures/instrument/index.js
+++ tests/fixtures/instrument/index.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-import { render } from 'untool';
+import { render } from '@untool/core';
 
 export default render(<h1>meep.</h1>);
