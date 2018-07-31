29109ba9eb6b032734c9ed974c50e6f65ddcb388
dmbch <daniel@dmbch.net>
2018-07-31 23:39:55 +0200

    feat(untool): introduce simple logging

--- packages/cli/index.js
+++ packages/cli/index.js
@@ -9,7 +9,7 @@ const { sync: resolve } = require('enhanced-resolve');
 try {
   const pkgFile = findUp('package.json');
   const yargsPath = resolve(dirname(pkgFile), '@untool/yargs');
-  require(yargsPath).run();
+  require(yargsPath).run({ mixins: [__dirname] });
 } catch (_) {
   // eslint-disable-next-line no-console
   console.error("Error: Can't find @untool/yargs \n");
