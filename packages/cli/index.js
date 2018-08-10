e223374068b4c12443fdc3d2e418a97d10143fec
dmbch <daniel@dmbch.net>
2018-08-10 20:50:26 +0200

    refactor(untool): improve error handling

--- packages/cli/index.js
+++ packages/cli/index.js
@@ -6,14 +6,25 @@ const { dirname } = require('path');
 const { sync: findUp } = require('find-up');
 const { sync: resolve } = require('enhanced-resolve');
 
-try {
+const getRootDir = () => {
   const pkgFile = findUp('package.json');
-  const yargsPath = resolve(dirname(pkgFile), '@untool/yargs');
+  if (!pkgFile) throw new Error("Can't resolve package.json");
+  return dirname(pkgFile);
+};
+
+try {
+  const rootDir = getRootDir();
+  const yargsPath = resolve(rootDir, '@untool/yargs');
   require(yargsPath)
     .configure({ mixins: [__dirname] })
     .run();
-} catch (_) {
-  // eslint-disable-next-line no-console
-  console.error("Error: Can't find @untool/yargs \n");
+} catch (error) {
+  if (error.message && error.message.startsWith("Can't resolve")) {
+    // eslint-disable-next-line no-console
+    console.error("Error: Can't find @untool/yargs \n");
+  } else {
+    // eslint-disable-next-line no-console
+    console.error(error.stack ? error.stack.toString() : error.toString());
+  }
   process.exit(1);
 }
