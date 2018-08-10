0359518801601a7c3e5645461041b569bc913144
dmbch <daniel@dmbch.net>
2018-08-10 22:33:18 +0200

    feat(untool): enable option to silence log output

--- packages/cli/mixin.core.js
+++ packages/cli/mixin.core.js
@@ -6,44 +6,70 @@ module.exports = class CLIMixin {
   constructor(config) {
     this.config = config;
   }
+  registerCommands(yargs) {
+    return yargs.option('quiet', {
+      alias: 'q',
+      description: 'Silence log output',
+      type: 'boolean',
+    });
+  }
+  handleArguments(argv) {
+    this.options = { ...this.options, ...argv };
+    const { quiet } = this.options;
+    const { name } = this.config;
+    if (!quiet) {
+      process.stdout.write(
+        `[${name}] started in ${process.env.NODE_ENV || 'development'} mode\n`
+      );
+    }
+  }
   configureBuild(webpackConfig, loaderConfigs, target) {
     if (target === 'develop') {
+      const { quiet } = this.options;
       const { name } = this.config;
-      webpackConfig.plugins.push({
-        apply(compiler) {
-          compiler.hooks.done.tap('untool-log-plugin', (stats) => {
-            process.stdout.write(
-              `${name} built successfully in ${prettyMS(
-                stats.endTime - stats.startTime
-              )}\n`
-            );
-          });
-        },
-      });
+      if (!quiet) {
+        webpackConfig.plugins.push({
+          apply(compiler) {
+            compiler.hooks.done.tap('untool-log-plugin', (stats) => {
+              process.stdout.write(
+                `[${name}] built successfully in ${prettyMS(
+                  stats.endTime - stats.startTime
+                )}\n`
+              );
+            });
+          },
+        });
+      }
     }
     return webpackConfig;
   }
   inspectBuild(stats) {
+    const { quiet } = this.options;
     const { name } = this.config;
-    process.stdout.write(
-      `${name} built successfully\n\n${stats.toString({
-        colors: false,
-        version: false,
-        hash: false,
-        modules: false,
-        entrypoints: false,
-        chunks: false,
-      })}\n\n`
-    );
+    if (!quiet) {
+      process.stdout.write(
+        `[${name}] built successfully\n\n${stats.toString({
+          colors: false,
+          version: false,
+          hash: false,
+          modules: false,
+          entrypoints: false,
+          chunks: false,
+        })}\n\n`
+      );
+    }
   }
   inspectServer(server) {
+    const { quiet } = this.options;
     const { name, https, basePath: pathname } = this.config;
-    const { address, port } = server.address();
-    const hostname = ['::', '::1', '0.0.0.0', '127.0.0.1'].includes(address)
-      ? 'localhost'
-      : address;
-    const protocol = https ? 'https' : 'http';
-    const parts = { protocol, hostname, port, pathname };
-    process.stdout.write(`${name} listening at ${format(parts)}\n`);
+    if (!quiet) {
+      const { address, port } = server.address();
+      const hostname = ['::', '::1', '0.0.0.0', '127.0.0.1'].includes(address)
+        ? 'localhost'
+        : address;
+      const protocol = https ? 'https' : 'http';
+      const parts = { protocol, hostname, port, pathname };
+      process.stdout.write(`[${name}] listening at ${format(parts)}\n`);
+    }
   }
 };
