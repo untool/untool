0a1131025e0682610546c2079310cb5ba2697fbe
dmbch <daniel@dmbch.net>
2018-08-12 01:08:51 +0200

    feat(defaults): introduce (configurable) exports

--- packages/defaults/index.js
+++ packages/defaults/index.js
@@ -1,3 +1,11 @@
 'use strict';
 
-module.exports = null;
+const configure = (...args) =>
+  Object.assign(exports, {
+    ...require('@untool/yargs').configure(...args),
+    ...require('@untool/express').configure(...args),
+    ...require('@untool/webpack').configure(...args),
+    ...require('@untool/react').configure(...args),
+    configure,
+  });
+configure();
