9fee3d548871a14237c57bec09ec81744d13dfdc
dmbch <daniel@dmbch.net>
2018-08-12 01:11:11 +0200

    refactor(express): make exports configurable

--- packages/express/index.js
+++ packages/express/index.js
@@ -1,27 +1,29 @@
 'use strict';
 
-module.exports = {
-  get uri() {
-    return require('./lib/uri');
-  },
-  runServer(mode, options = {}) {
-    const core = require('@untool/core').bootstrap();
-    core.handleArguments(options);
-    return core.runServer(mode);
-  },
-  createServer(mode, options = {}) {
-    const core = require('@untool/core').bootstrap();
-    core.handleArguments(options);
-    return core.createServer(mode);
-  },
-  createRenderer(options = {}) {
-    const core = require('@untool/core').bootstrap();
-    core.handleArguments(options);
-    return core.createRenderer();
-  },
-  renderLocations(options = {}) {
-    const core = require('@untool/core').bootstrap();
-    core.handleArguments(options);
-    return core.renderLocations();
-  },
-};
+exports.uri = require('./lib/uri');
+
+const configure = (config = {}, options = {}) =>
+  Object.assign(exports, {
+    runServer(...args) {
+      const core = require('@untool/core').bootstrap(config);
+      core.handleArguments(options);
+      return core.runServer(...args);
+    },
+    createServer(...args) {
+      const core = require('@untool/core').bootstrap(config);
+      core.handleArguments(options);
+      return core.createServer(...args);
+    },
+    createRenderer(...args) {
+      const core = require('@untool/core').bootstrap(config);
+      core.handleArguments(options);
+      return core.createRenderer(...args);
+    },
+    renderLocations(...args) {
+      const core = require('@untool/core').bootstrap(config);
+      core.handleArguments(options);
+      return core.renderLocations(...args);
+    },
+    configure,
+  });
+configure();
