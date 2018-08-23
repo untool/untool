baca97064c664c9ce876774d4386a082208153ef
dmbch <daniel@dmbch.net>
2018-08-23 21:09:54 +0200

    refactor(webpack): simplify, improve legibility

--- packages/webpack/lib/plugins/assets.js
+++ packages/webpack/lib/plugins/assets.js
@@ -10,7 +10,6 @@ module.exports = class WebpackAssetsPlugin {
   }
   apply(compiler) {
     compiler.hooks.emit.tap('untool-assets', (compilation) => {
-      const assets = Object.keys(compilation.assets);
       const chunkNames = Array.from(compilation.namedChunks.keys());
       const assetsByChunkName = chunkNames.reduce(
         (result, key) => ({
@@ -20,15 +19,16 @@ module.exports = class WebpackAssetsPlugin {
         {}
       );
       if (this.options.static) {
-        this.setAssets(assets, assetsByChunkName);
+        this.setAssets(assetsByChunkName);
       } else {
         compilation.assets[this.config.assetFile] = new RawSource(
-          JSON.stringify({
-            assets: assets,
-            assetsByChunkName: assetsByChunkName,
-          })
+          JSON.stringify(assetsByChunkName)
         );
       }
+    });
+    compiler.hooks.done.tap('untool-assets', (stats) => {
+      const { assetsByChunkName } = stats.toJson();
+      this.setAssets(assetsByChunkName);
     });
   }
 };
