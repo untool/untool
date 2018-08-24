86c7100d9e95597d86412f5dda8650b5ab53532f
dmbch <daniel@dmbch.net>
2018-08-25 00:10:40 +0200

    refactor(webpack): fix dir resolution

--- packages/webpack/preset.js
+++ packages/webpack/preset.js
@@ -1,9 +1,11 @@
 'use strict';
 
+const { join } = require('path');
+
 module.exports = {
   browsers: 'defaults',
   node: 'current',
-  serverDir: '<buildDir>',
+  serverDir: join('<rootDir>', 'node_modules', '.cache', 'untool'),
   serverFile: 'server.js',
   assetFile: 'assets.json',
   mixins: [__dirname],
