46a69ca888db9ae0a723cdf4126582664e9338a1
dmbch <daniel@dmbch.net>
2018-09-10 23:29:12 +0200

    fix(webpack): clone asset metadata in middleware
    closes: #132

--- packages/webpack/lib/middleware/assets.js
+++ packages/webpack/lib/middleware/assets.js
@@ -1,10 +1,12 @@
 'use strict';
 
+const clone = require('clone');
+
 module.exports = ({ assets: resolvable }) => {
   return function assetDataMiddleware(req, res, next) {
     resolvable
       .then((assets) => {
-        res.locals = { ...res.locals, ...assets };
+        res.locals = { ...res.locals, ...clone(assets, false) };
         next();
       })
       .catch(next);
