b91209b3227b7fce8479e5f8d313c0329cc3eea0
dmbch <daniel@dmbch.net>
2018-09-11 23:58:01 +0200

    fix(webpack): limit per-request memory
    ...by getting rid of stats cloning

--- packages/webpack/lib/middleware/stats.js
+++ packages/webpack/lib/middleware/stats.js
@@ -1,12 +1,10 @@
 'use strict';
 
-const clone = require('clone');
-
 module.exports = function createStatsMiddleware({ stats: resolvable }) {
   return function statsMiddleware(req, res, next) {
     resolvable
       .then((stats) => {
-        res.locals = { ...res.locals, stats: clone(stats, false) };
+        res.locals = { ...res.locals, stats };
         next();
       })
       .catch(next);
