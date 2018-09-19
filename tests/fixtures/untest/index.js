5f4898f3b2dd9486c6ee3c909053ae84bd7a0a95
dmbch <daniel@dmbch.net>
2018-09-19 23:17:38 +0200

    test: update fixture to demonstrate code splitting

--- tests/fixtures/untest/index.js
+++ tests/fixtures/untest/index.js
@@ -1,5 +1,26 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react';
-import { render } from '@untool/core';
+/* eslint-disable no-unused-vars */
+import React, { Fragment } from 'react';
+import { Helmet } from 'react-helmet';
+import { Switch, Route, Link } from 'react-router-dom';
 
-export default render(<h1>meep.</h1>);
+import { render } from '@untool/core';
+import { Import, Miss } from '@untool/react';
+
+const Home = Import('./home');
+const About = Import('./about');
+
+const App = () => (
+  <Fragment>
+    <Helmet>
+      <title>Untest</title>
+    </Helmet>
+    <Link to="/">Home</Link> <Link to="/about">About</Link>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Miss />
+    </Switch>
+  </Fragment>
+);
+
+export default render(<App />);
