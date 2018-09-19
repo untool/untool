a7f316f8fe26401ef37a50a8be664cf6f6880912
dmbch <daniel@dmbch.net>
2018-09-19 23:15:19 +0200

    refactor(webpack): improve asset/chunk handling

--- packages/webpack/lib/plugins/stats.js
+++ packages/webpack/lib/plugins/stats.js
@@ -1,30 +1,23 @@
 'use strict';
 
-const { extname } = require('path');
-
 const { RawSource } = require('webpack-sources');
 
 exports.StatsPlugin = class WebpackStatsPlugin {
-  constructor({ stats: resolvable, config: { name: entry } }) {
+  constructor({ stats: resolvable }) {
     this.apply = (compiler) => {
       compiler.hooks.emit.tap('untool-stats', (compilation) => {
         const stats = compilation.getStats().toJson();
-        const entryAssetsByType = Object.entries(stats.assetsByChunkName)
-          .filter(([name]) => new RegExp(`^(vendors~)?${entry}$`).test(name))
-          .sort(([name]) => (name.startsWith('vendors~') ? 0 : 1))
-          .map(([, assets]) => (Array.isArray(assets) ? assets : [assets]))
-          .reduce(
-            (result, assets) =>
-              assets.reduce((result, asset) => {
-                const extension = extname(asset).substring(1);
-                if (result[extension] && !asset.endsWith('.hot-update.js')) {
-                  result[extension].push(asset);
-                }
-                return result;
-              }, result),
-            { css: [], js: [] }
-          );
-        resolvable.resolve({ ...stats, entryAssetsByType });
+        const moduleIds = {};
+        compilation.chunks.forEach((chunk) => {
+          for (const module of chunk.modulesIterable) {
+            let currentModule = module;
+            if (module.constructor.name === 'ConcatenatedModule') {
+              currentModule = module.rootModule;
+            }
+            moduleIds[currentModule.rawRequest] = module.id;
+          }
+        });
+        resolvable.resolve({ ...stats, moduleIds });
       });
       compiler.hooks.watchRun.tap('untool-stats', () => resolvable.reset());
     };
