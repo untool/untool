6b7a1dea9da03487c6ce578f14073f8226d38418
dmbch <daniel@dmbch.net>
2018-09-22 20:24:38 +0200

    feat(webpack): add stats data needed for code splitting

--- packages/webpack/lib/plugins/stats.js
+++ packages/webpack/lib/plugins/stats.js
@@ -7,17 +7,40 @@ exports.StatsPlugin = class WebpackStatsPlugin {
     this.apply = (compiler) => {
       compiler.hooks.emit.tap('untool-stats', (compilation) => {
         const stats = compilation.getStats().toJson();
-        const moduleIds = {};
+        const moduleIdMap = {};
+        const moduleChunkMap = {};
         compilation.chunks.forEach((chunk) => {
           for (const module of chunk.modulesIterable) {
             let currentModule = module;
             if (module.constructor.name === 'ConcatenatedModule') {
               currentModule = module.rootModule;
             }
-            moduleIds[currentModule.rawRequest] = module.id;
+            moduleIdMap[currentModule.rawRequest] = module.id;
+            moduleChunkMap[module.id] = chunk.id;
           }
         });
-        resolvable.resolve({ ...stats, moduleIds });
+        const moduleDepIds = stats.modules.reduce(
+          (result, { id }) => ({
+            ...result,
+            [id]: (function getAllModuleIds(moduleIds) {
+              const depModuleIds = stats.modules.reduce(
+                (result, { id, reasons }) =>
+                  !moduleIds.includes(id) &&
+                  reasons.find(({ moduleId }) => moduleIds.includes(moduleId))
+                    ? result.concat(id)
+                    : result,
+                []
+              );
+              if (depModuleIds.length) {
+                return getAllModuleIds(moduleIds.concat(depModuleIds));
+              }
+              return moduleIds;
+            })([id]),
+          }),
+          {}
+        );
+        const additions = { moduleIdMap, moduleChunkMap, moduleDepIds };
+        resolvable.resolve({ ...stats, ...additions });
       });
       compiler.hooks.watchRun.tap('untool-stats', () => resolvable.reset());
     };
