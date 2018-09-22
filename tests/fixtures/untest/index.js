aece242caca7842d80e5698ce5d42142da802182
dmbch <daniel@dmbch.net>
2018-09-22 18:17:35 +0200

    test: make fixture more comprehensive

--- tests/fixtures/untest/index.js
+++ tests/fixtures/untest/index.js
@@ -1,13 +1,31 @@
 /* eslint-disable no-unused-vars */
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Helmet } from 'react-helmet';
-import { Switch, Route, Link } from 'react-router-dom/es';
+import { Switch, Route, Link } from 'react-router-dom';
 
 import { render } from '@untool/core';
 import { Import, Miss } from '@untool/react';
 
 const Home = Import('./home');
 const About = Import('./about');
+
+class Placeholder extends Component {
+  constructor({ load }) {
+    super();
+    this.state = { Component: () => <p>Loading...</p> };
+    new Promise((resolve) => setTimeout(resolve, 500)).then(() =>
+      load().then(
+        ({ default: Component }) => this.setState({ Component }),
+        () => this.setState({ Component: () => <p>Error...</p> })
+      )
+    );
+  }
+  render() {
+    const { props, state } = this;
+    const { Component } = state;
+    return <Component {...props} />;
+  }
+}
 
 const App = () => (
   <Fragment>
@@ -16,8 +34,8 @@ const App = () => (
     </Helmet>
     <Link to="/">Home</Link> <Link to="/about">About</Link>
     <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
+      <Route exact path="/" component={Home} />} />
+      <Route path="/about" render={() => <About placeholder={Placeholder} />} />
       <Miss />
     </Switch>
   </Fragment>
