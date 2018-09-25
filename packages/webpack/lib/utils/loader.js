5ee37e80b95254664b2612bb7867e8d1ff9962b6
dmbch <daniel@dmbch.net>
2018-09-26 00:59:11 +0200

    refactor(webpack): re-implement and re-organize

--- packages/webpack/lib/utils/loader.js
+++ packages/webpack/lib/utils/loader.js
@@ -2,27 +2,27 @@
 
 const { getOptions } = require('loader-utils');
 
-const getMixins = (target, config) =>
-  `[${(config.mixins[target] || [])
+const getMixins = (type, config) =>
+  `[${(config.mixins[type] || [])
     .map(
       (mixin) => `(function(m) { return m.default || m })(require('${mixin}'))`
     )
     .join(',')}]`;
 
-const getConfig = (target, config) =>
+const getConfig = (type, config) =>
   JSON.stringify(
     Object.assign({}, config._config, { mixins: undefined })
   ).replace(
     new RegExp(`"${config.rootDir}(.*?)"`, 'g'),
-    target === 'server' ? 'expand(".$1")' : '".$1"'
+    type === 'server' ? 'expand(".$1")' : '".$1"'
   );
 
 const getConfigAndMixins = (...args) =>
   `{ config: ${getConfig(...args)}, mixins: ${getMixins(...args)} }`;
 
-const getModule = (target, config) => {
-  const configAndMixins = getConfigAndMixins(target, config);
-  return (target === 'server'
+const getModule = (type, config) => {
+  const configAndMixins = getConfigAndMixins(type, config);
+  return (type === 'server'
     ? [
         'var path = require("path");',
         'var root = path.dirname(require("find-up").sync("package.json"));',
@@ -38,6 +38,6 @@ const getModule = (target, config) => {
 
 module.exports = function() {
   this.cacheable();
-  const { target, config } = getOptions(this);
-  return getModule(target, config);
+  const { type, config } = getOptions(this);
+  return getModule(type, config);
 };
