d7c53f364b8f28f5f49f7e3b099aceff4655244f
dmbch <daniel@dmbch.net>
2018-09-27 15:08:42 +0200

    fix(webpack): re-implement stats analysis

--- packages/webpack/lib/plugins/stats.js
+++ packages/webpack/lib/plugins/stats.js
@@ -6,64 +6,52 @@ exports.StatsPlugin = class WebpackStatsPlugin {
   constructor({ stats: resolvable }) {
     this.apply = (compiler) => {
       compiler.hooks.emit.tap('untool-stats', (compilation) => {
-        const stats = compilation.getStats().toJson();
-        const moduleIdMap = {};
-        const moduleChunkMap = {};
-        compilation.chunks.forEach((chunk) => {
-          for (const module of chunk.modulesIterable) {
-            let currentModule = module;
-            if (module.constructor.name === 'ConcatenatedModule') {
-              currentModule = module.rootModule;
+        try {
+          const stats = compilation.getStats().toJson();
+          const moduleChunkGroupMap = {};
+          const chunkGroups = compilation.chunkGroups.reduce(
+            (result, { chunks }) => [...result, chunks],
+            []
+          );
+          compilation.chunks.forEach((chunk) => {
+            for (const module of chunk.modulesIterable) {
+              let currentModule = module;
+              if (module.constructor.name === 'ConcatenatedModule') {
+                currentModule = module.rootModule;
+              }
+              moduleChunkGroupMap[currentModule.rawRequest] = chunkGroups.find(
+                (group) => group.find(({ id }) => chunk.id === id)
+              );
             }
-            moduleIdMap[currentModule.rawRequest] = module.id;
-            moduleChunkMap[module.id] = chunk;
-          }
-        });
-        const getDepModuleIds = (id) =>
-          (function getAllModuleIds(moduleIds) {
-            const depModuleIds = stats.modules.reduce(
-              (result, { id, reasons, chunks }) =>
-                !moduleIds.includes(id) &&
-                chunks.length &&
-                reasons.find(({ moduleId }) => moduleIds.includes(moduleId))
-                  ? result.concat(id)
-                  : result,
-              []
-            );
-            if (depModuleIds.length) {
-              return getAllModuleIds(moduleIds.concat(depModuleIds));
-            }
-            return moduleIds;
-          })([id]);
-        const moduleFileMap = Object.keys(moduleIdMap).reduce(
-          (result, rawRequest) => ({
-            ...result,
-            [rawRequest]: getDepModuleIds(moduleIdMap[rawRequest])
-              .reduce(
-                (result, moduleId) =>
-                  result.concat(moduleChunkMap[moduleId].files),
-                []
-              )
-              .filter((file, index, self) => self.indexOf(file) === index),
-          }),
-          {}
-        );
-        const entryChunks = stats.chunks.filter(({ entry }) => entry);
-        const vendorChunks = stats.chunks.filter(({ id }) =>
-          entryChunks.find(({ siblings }) => siblings.includes(id))
-        );
-        const entryFiles = entryChunks.reduce(
-          (result, { files }) => result.concat(files),
-          []
-        );
-        const vendorFiles = vendorChunks.reduce(
-          (result, { files }) => result.concat(files),
-          []
-        );
-        resolvable.resolve({
-          ...stats,
-          ...{ moduleFileMap, entryFiles, vendorFiles },
-        });
+          });
+          const moduleFileMap = Object.keys(moduleChunkGroupMap).reduce(
+            (result, rawRequest) => ({
+              ...result,
+              [rawRequest]: (moduleChunkGroupMap[rawRequest] || [])
+                .reduce((result, { files }) => [...result, ...files], [])
+                .filter((file, index, self) => self.indexOf(file) === index),
+            }),
+            {}
+          );
+          const entryChunks = stats.chunks.filter(({ entry }) => entry);
+          const vendorChunks = stats.chunks.filter(({ id }) =>
+            entryChunks.find(({ siblings }) => siblings.includes(id))
+          );
+          const entryFiles = entryChunks.reduce(
+            (result, { files }) => result.concat(files),
+            []
+          );
+          const vendorFiles = vendorChunks.reduce(
+            (result, { files }) => result.concat(files),
+            []
+          );
+          resolvable.resolve({
+            ...stats,
+            ...{ moduleFileMap, entryFiles, vendorFiles },
+          });
+        } catch (error) {
+          resolvable.reject(error);
+        }
       });
       compiler.hooks.watchRun.tap('untool-stats', () => resolvable.reset());
     };
