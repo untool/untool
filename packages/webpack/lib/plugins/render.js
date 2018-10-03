44fdb545a806909efeb215dbb35f02d53ba4b94d
dmbch <daniel@dmbch.net>
2018-10-04 00:58:05 +0200

    refactor(webpack): make render plugin self-contained
    ...add settings previously removed from `@untool/express` and change
    hook strategy on `configureBuild` to `sequence`.

--- packages/webpack/lib/plugins/render.js
+++ packages/webpack/lib/plugins/render.js
@@ -1,18 +1,33 @@
 'use strict';
 
+const indexFile = require('directory-index');
 const { RawSource } = require('webpack-sources');
 
-module.exports = class RenderPlugin {
-  constructor({ renderLocations }) {
-    this.apply = (compiler) =>
-      compiler.hooks.compilation.tap('RenderPlugin', (compilation) =>
-        compilation.hooks.additionalAssets.tapPromise('RenderPlugin', () =>
-          renderLocations().then((pages) =>
-            Object.entries(pages).forEach(
-              ([path, page]) => (compilation.assets[path] = new RawSource(page))
-            )
-          )
-        )
+const {
+  internal: {
+    uri: { resolveAbsolute, resolveRelative },
+  },
+} = require('@untool/express');
+
+exports.RenderPlugin = class RenderPlugin {
+  constructor({ createRenderer, config: { basePath, locations } }) {
+    const render = createRenderer();
+    this.apply = (compiler) => {
+      const toUrlPath = resolveAbsolute.bind(null, basePath);
+      const toFsPath = resolveRelative.bind(null, basePath);
+      const promise = Promise.all(
+        locations.map((location) => render(toUrlPath(location)))
       );
+      compiler.hooks.compilation.tap('RenderPlugin', (compilation) => {
+        compilation.hooks.additionalAssets.tapPromise('RenderPlugin', () =>
+          promise.then((responses) =>
+            responses.forEach((response, index) => {
+              const path = toFsPath(indexFile(locations[index]));
+              compilation.assets[path] = new RawSource(response);
+            })
+          )
+        );
+      });
+    };
   }
 };
--- packages/webpack/preset.js
+++ packages/webpack/preset.js
@@ -5,6 +5,10 @@ const { join } = require('path');
 module.exports = {
   browsers: 'defaults',
   node: 'current',
+  locations: [],
+  basePath: '',
+  assetPath: '<basePath>',
+  buildDir: '<distDir>',
   serverDir: join('<rootDir>', 'node_modules', '.cache', 'untool'),
   serverFile: 'server.js',
   statsFile: 'stats.json',
