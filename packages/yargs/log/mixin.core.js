1b42b9a05daf97abde85d0201ee8f5ba605e286d
dmbch <daniel@dmbch.net>
2018-10-03 17:30:04 +0200

    feat(yargs): add optional logging mixin

---
+++ packages/yargs/log/mixin.core.js
@@ -0,0 +1,68 @@
+'use strict';
+/* eslint-disable no-console */
+
+const { format } = require('url');
+
+const prettyMS = require('pretty-ms');
+
+const { Mixin } = require('@untool/core');
+
+module.exports = class CLIMixin extends Mixin {
+  registerCommands(yargs) {
+    return yargs.option('quiet', {
+      alias: 'q',
+      description: 'Silence log output',
+      type: 'boolean',
+    });
+  }
+  handleArguments(argv) {
+    this.options = { ...this.options, ...argv };
+    const { quiet } = this.options;
+    if (!quiet) {
+      const { name } = this.config;
+      const mode = argv.production ? 'production' : 'development';
+      console.log(`[${name}] started in ${mode} mode`);
+    }
+  }
+  configureBuild(webpackConfig, loaderConfigs, target) {
+    const { quiet } = this.options;
+    if (!quiet && target === 'develop') {
+      const { name } = this.config;
+      webpackConfig.plugins.push({
+        apply(compiler) {
+          compiler.hooks.done.tap('LogPlugin', ({ endTime, startTime }) => {
+            const time = prettyMS(endTime - startTime);
+            console.log(`[${name}] built successfully in ${time}`);
+          });
+        },
+      });
+    }
+    return webpackConfig;
+  }
+  inspectBuild(stats) {
+    const { quiet } = this.options;
+    if (!quiet) {
+      const { name } = this.config;
+      const report = stats.toString({
+        chunks: false,
+        colors: false,
+        entrypoints: false,
+        hash: false,
+        modules: false,
+        version: false,
+      });
+      console.log(`[${name}] built successfully\n\n${report}\n`);
+    }
+  }
+  inspectServer(server) {
+    const { quiet } = this.options;
+    if (!quiet) {
+      const { name, https, basePath: pathname } = this.config;
+      const { port } = server.address();
+      const hostname = 'localhost';
+      const protocol = https ? 'https' : 'http';
+      const parts = { protocol, hostname, port, pathname };
+      console.log(`[${name}] listening at ${format(parts)}`);
+    }
+  }
+};
