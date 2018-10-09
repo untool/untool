30dfd4e5604d589edae8b5e29687fd46973adc25
dmbch <daniel@dmbch.net>
2018-10-10 00:05:07 +0200

    refactor(webpack): improve loader implementation

--- packages/webpack/lib/utils/loader.js
+++ packages/webpack/lib/utils/loader.js
@@ -2,35 +2,37 @@
 
 const { getOptions } = require('loader-utils');
 
-// eslint-disable-next-line no-unused-vars
-const getConfig = (type, { _config: { mixins: _, ...config } }) =>
+const getConfig = (type, { _config: config }) =>
   JSON.stringify(config).replace(
     new RegExp(`"${config.rootDir}(.*?)"`, 'g'),
     type === 'server' ? 'expand(".$1")' : '".$1"'
   );
 
-const getMixins = (type, { mixins }) =>
-  `[${(mixins[type] || [])
-    .map((mixin) => `((m) => m.default || m )(require('${mixin}'))`)
-    .join(',')}]`;
+const getMixins = (type, { _mixins: mixins }) => {
+  const requires = (mixins[type] || []).map(
+    (mixin) => `((m) => m.default || m )(require('${mixin}'))`
+  );
+  return `[${requires.join(',')}]`;
+};
 
 module.exports = function configLoader() {
   this.cacheable();
   const { type, config } = getOptions(this);
   return [
+    'const utils = require("@untool/core/lib/utils");',
     ...(type === 'server'
       ? [
-          'const path = require("path");',
-          'const findUp = require("find-up").sync;',
-          'const root = path.dirname(findUp("package.json"));',
-          'const expand = path.join.bind(path, root);',
+          'const { dirname, join } = require("path");',
+          'const { sync: findUp } = require("find-up");',
+          'const root = dirname(findUp("package.json"));',
+          'const expand = (path) => join(root, path);',
         ]
       : []),
-    `const { environmentalize, placeholdify, merge } = require('./utils');`,
-    `const override = merge.bind(null, ${getConfig(type, config)});`,
-    `const functions = [override, placeholdify, environmentalize];`,
-    `const getConfig = functions.reduce.bind(functions, (res, fn) => fn(res));`,
-    `exports.getConfig = (overrides = {}) => getConfig(overrides);`,
+    'const { environmentalize, placeholdify, merge } = utils;',
+    `const override = (...args) => merge(${getConfig(type, config)}, ...args);`,
+    'const functions = [override, placeholdify, environmentalize];',
+    'const getConfig = [].reduce.bind(functions, (res, fn) => fn(res));',
+    'exports.getConfig = (overrides = {}) => getConfig(overrides);',
     `exports.getMixins = () => ${getMixins(type, config)};`,
   ].join('\n');
 };
