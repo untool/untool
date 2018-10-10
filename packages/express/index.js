e62462361a184a0b1bc0bcdbe3a30d6d30ae2034
ZauberNerd <zaubernerd@zaubernerd.de>
2018-10-10 17:00:23 +0200

    fix(express): remove typo in import name (intialize -> initialize)

--- packages/express/index.js
+++ packages/express/index.js
@@ -1,18 +1,18 @@
 'use strict';
 
-const { intialize } = require('@untool/core');
+const { initialize } = require('@untool/core');
 
 const uri = require('./lib/uri');
 
 const configure = (config, options) => ({
   runServer(...args) {
-    return intialize(config, options).runServer(...args);
+    return initialize(config, options).runServer(...args);
   },
   createServer(...args) {
-    return intialize(config, options).createServer(...args);
+    return initialize(config, options).createServer(...args);
   },
   createRenderer(...args) {
-    return intialize(config, options).createRenderer(...args);
+    return initialize(config, options).createRenderer(...args);
   },
   configure,
   internal: { uri },
