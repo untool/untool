6115e236beb0539edbd49fcde1c233b44a7c7317
dmbch <daniel@dmbch.net>
2018-10-15 17:01:03 +0200

    fix(webpack): support comma separated browserslist queries

--- packages/webpack/lib/middlewares/agent.js
+++ packages/webpack/lib/middlewares/agent.js
@@ -2,11 +2,14 @@
 
 const { matchesUA } = require('browserslist-useragent');
 
+const toArray = (str) =>
+  str.split(',').map((item) => item.replace(/(?:^\s+|\s+$)/g, ''));
+
 module.exports = function createAgentMiddleware(browsers) {
   return function agentMiddleware(req, res, next) {
     const userAgent = req.headers['user-agent'] || '';
     const options = {
-      browsers: typeof browsers === 'string' ? [browsers] : browsers,
+      browsers: typeof browsers === 'string' ? toArray(browsers) : browsers,
       ignorePatch: true,
       ignoreMinor: true,
       allowHigherVersions: true,
