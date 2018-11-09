014459bedb352b9ba5003684045a9d9168a4a591
ZauberNerd <zaubernerd@zaubernerd.de>
2018-11-09 13:10:34 +0100

    fix(react): do not render "undefined" as a value in the template
    In case `fragments` is not defined or an empty object we should ensure
    that we don't render `undefined` in the template.
    This change adds `|| ''` and sets a default value of an empty object for
    `fragments`, so we will always render strings only.

--- packages/react/lib/template.js
+++ packages/react/lib/template.js
@@ -8,28 +8,28 @@ const renderCSS = ({ css }) =>
 const renderJS = ({ js }) =>
   js.map((asset) => `<script src="/${asset}"></script>`).join('');
 
-const renderGlobals = (globals) => {
+const renderGlobals = (globals = {}) => {
   const entries = Object.entries(globals).map(([k, v]) => `${k}=${esc(v)}`);
   return entries.length ? `<script>var ${entries.join(',')};</script>` : '';
 };
 
-module.exports = ({ fragments, assets, mountpoint, markup, globals }) =>
+module.exports = ({ fragments = {}, assets, mountpoint, markup, globals }) =>
   `<!DOCTYPE html>
-<html ${fragments.htmlAttributes}>
+<html ${fragments.htmlAttributes || ''}>
   <head>
-    ${fragments.headPrefix}
-    ${fragments.title}
-    ${fragments.base}
-    ${fragments.meta}
-    ${fragments.link}
+    ${fragments.headPrefix || ''}
+    ${fragments.title || ''}
+    ${fragments.base || ''}
+    ${fragments.meta || ''}
+    ${fragments.link || ''}
     ${renderCSS(assets)}
-    ${fragments.style}
-    ${fragments.script}
-    ${fragments.headSuffix}
+    ${fragments.style || ''}
+    ${fragments.script || ''}
+    ${fragments.headSuffix || ''}
   </head>
-  <body ${fragments.bodyAttributes}>
-    <div id="${mountpoint}">${markup}</div>
-    ${fragments.noscript}
+  <body ${fragments.bodyAttributes || ''}>
+    <div id="${mountpoint}">${markup || ''}</div>
+    ${fragments.noscript || ''}
     ${renderGlobals(globals)}
     ${renderJS(assets)}
   </body>
