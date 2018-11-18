b8c693443bbc98279d715a806d047a3e7c763884
dmbch <daniel@dmbch.net>
2018-11-19 00:25:30 +0100

    fix(webpack): prevent dev server crashing on error

--- packages/webpack/lib/middlewares/render.js
+++ packages/webpack/lib/middlewares/render.js
@@ -4,61 +4,70 @@ const { join } = require('path');
 
 const webpack = require('webpack');
 const MemoryFS = require('memory-fs');
+
 const sourceMapSupport = require('source-map-support');
 const requireFromString = require('require-from-string');
+
 const EnhancedPromise = require('eprom');
 
-module.exports = function createRenderMiddleware(webpackConfig, watch) {
+const extractErrors = (stats) => {
+  const { errors } = stats.toJson();
+  const details = errors.map((error) =>
+    error.replace(/\033?\[[0-9]{1,2}m/g, '')
+  );
+  return details.join('\n');
+};
+
+const getBuildPromise = (webpackConfig) => {
+  const compiler = webpack(webpackConfig);
+  compiler.outputFileSystem = new MemoryFS();
   sourceMapSupport.install({ environment: 'node', hookRequire: true });
-
-  let middleware = null;
-
-  const enhancedPromise = new EnhancedPromise((resolve, reject, reset) => {
-    const handleCompilation = (compileError, stats) => {
+  return new Promise((resolve, reject) => {
+    compiler.run((compileError, stats) => {
       if (compileError) {
         reject(compileError);
       } else if (stats.hasErrors()) {
-        const { errors } = stats.toJson();
-        const details = errors.map((error) =>
-          error.replace(/\033?\[[0-9]{1,2}m/g, '')
-        );
-        reject(new Error(`Can't compile:\n${details.join('\n')}`));
+        reject(new Error(`Can't compile:\n${extractErrors(stats)}`));
       } else {
         const { path, filename } = webpackConfig.output;
         const filePath = join(path, filename);
-        if (watch) {
-          if (middleware) {
-            process.kill(process.pid, 'SIGUSR2');
-            resolve(middleware);
-          } else {
-            try {
-              middleware = require(filePath).default;
-              resolve(middleware);
-            } catch (moduleError) {
-              reject(moduleError);
-            }
-          }
-        } else {
-          const { outputFileSystem: fs } = compiler;
-          fs.readFile(filePath, 'utf-8', (readError, fileContents) => {
-            if (readError) return reject(readError);
-            resolve(requireFromString(fileContents, filePath).default);
-          });
-        }
+        const { outputFileSystem: fs } = compiler;
+        fs.readFile(filePath, 'utf-8', (readError, fileContents) => {
+          if (readError) return reject(readError);
+          resolve(requireFromString(fileContents, filePath));
+        });
       }
-    };
-
-    const compiler = webpack(webpackConfig);
-
-    if (watch) {
-      compiler.hooks.watchRun.tap('RenderMiddleware', () => reset());
-      compiler.watch(webpackConfig.watchOptions, handleCompilation);
-    } else {
-      compiler.outputFileSystem = new MemoryFS();
-      compiler.run(handleCompilation);
-    }
+    });
   });
+};
 
+const getWatchPromise = (webpackConfig) => {
+  const compiler = webpack(webpackConfig);
+  let middleware = null;
+  return new EnhancedPromise((resolve, reject, reset) => {
+    compiler.hooks.watchRun.tap('RenderMiddleware', () => reset());
+    compiler.watch(webpackConfig.watchOptions, (compileError, stats) => {
+      if (compileError) {
+        reject(compileError);
+      } else if (stats.hasErrors()) {
+        reject(new Error(`Can't compile:\n${extractErrors(stats)}`));
+      } else {
+        if (middleware) {
+          process.kill(process.pid, 'SIGUSR2');
+        } else {
+          const { path, filename } = webpackConfig.output;
+          const filePath = join(path, filename);
+          middleware = require(filePath);
+        }
+        resolve(middleware);
+      }
+    });
+  });
+};
+
+module.exports = function createRenderMiddleware(webpackConfig, watch) {
+  const getPromise = watch ? getWatchPromise : getBuildPromise;
+  const enhancedPromise = getPromise(webpackConfig);
   return function renderMiddleware(req, res, next) {
     enhancedPromise
       .then((middleware) => middleware(req, res, next))
