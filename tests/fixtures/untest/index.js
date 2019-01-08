e062a2c61f8c82a39cf978721b8e990ed7494ef2
dmbch <daniel@dmbch.net>
2019-01-08 21:50:59 +0100

    test: add re-exports to fixture

--- tests/fixtures/untest/index.js
+++ tests/fixtures/untest/index.js
@@ -4,8 +4,8 @@ import { Switch, Route, Link } from 'react-router-dom/es';
 
 import { Miss, render, importComponent } from 'untool';
 
-const Home = importComponent('./home');
-const About = importComponent('./about', 'About');
+const Home = importComponent('./components/home');
+const About = importComponent('./components/about', 'About');
 
 const preload = ({ Component, error, loading, ...props }) => {
   if (loading) return <p>Loading...</p>;
