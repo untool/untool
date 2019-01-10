0b198e6366555ed94814e92c2af57e25ec588aa6
dmbch <daniel@dmbch.net>
2019-01-10 22:19:33 +0100

    feat(express): add graceful exception handling
    This commit introduces a new config option: `gracePeriod`. In case of
    uncaught asynchronous middleware errors or upon receival of a
    `SIGTERM` signal, the server's `close` method will now be called before
    exiting the process. If the server fails to finish answering pending
    requests before `gracePeriod` is over, it will be terminated.

--- packages/express/lib/serve.js
+++ packages/express/lib/serve.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const { create: createDomain } = require('domain');
+
 const debug = require('debug')('untool:express');
 const express = require('express');
 
@@ -16,19 +18,22 @@ module.exports = (mode, { configureServer }) => {
   );
   const app = express();
   const router = new Router();
+  const domain = createDomain();
 
   configureServer(app, middlewares, mode);
-  app.use(router);
-
   debug(middlewares);
 
+  app.use(router);
   phases.forEach((phase) =>
     middlewares[phase].forEach((middleware) =>
       (/final/.test(phase) ? app : router).use(
-        ...(Array.isArray(middleware) ? middleware : [middleware])
+        ...(Array.isArray(middleware) ? middleware : [middleware]).map(
+          (middleware) => domain.bind(middleware)
+        )
       )
     )
   );
+  app.locals.domain = domain;
 
   return app;
 };
