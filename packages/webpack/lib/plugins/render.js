de2e1c172090834febdf031761026c81dcbf755a
dmbch <daniel@dmbch.net>
2019-01-22 13:20:37 +0100

    fix(webpack): prevent needless rendering

--- packages/webpack/lib/plugins/render.js
+++ packages/webpack/lib/plugins/render.js
@@ -8,22 +8,22 @@ const { trimLeadingSlash } = require('pathifist');
 exports.RenderPlugin = class RenderPlugin {
   constructor(render, requests) {
     this.apply = (compiler) => {
-      const promise = requests.then((requests) =>
-        Promise.all(
-          requests.map((request) =>
-            render(request).then((content) => ({
-              outfile: trimLeadingSlash(
-                request.outfile || indexFile(request.url || request)
-              ),
-              content,
-            }))
-          )
-        )
-      );
       compiler.hooks.compilation.tap('RenderPlugin', (compilation) => {
         if (compilation.compiler.isChild()) {
           return;
         }
+        const promise = requests.then((requests) =>
+          Promise.all(
+            requests.map((request) =>
+              render(request).then((content) => ({
+                outfile: trimLeadingSlash(
+                  request.outfile || indexFile(request.url || request)
+                ),
+                content,
+              }))
+            )
+          )
+        );
         compilation.hooks.additionalAssets.tapPromise('RenderPlugin', () =>
           promise.then((results) =>
             results.forEach(({ outfile, content }) => {
