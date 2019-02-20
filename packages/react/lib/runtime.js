9150954e52ff70de051a9df4d2b85dfe36d5d6f7
dmbch <daniel@dmbch.net>
2019-02-20 16:06:23 +0100

    feat(react): add arg/prop validation to public API

--- packages/react/lib/runtime.js
+++ packages/react/lib/runtime.js
@@ -1,79 +1,110 @@
 'use strict';
 /* global __webpack_modules__, __webpack_require__ */
 
-const { createElement, Component } = require('react');
+const { isValidElement, createElement, Component } = require('react');
 const { default: withRouter } = require('react-router-dom/es/withRouter');
+const isPlainObject = require('is-plain-object');
+const PropTypes = require('prop-types');
 
-const { initialize } = require('@untool/core');
+const {
+  initialize,
+  internal: { invariant },
+} = require('@untool/core');
 
 exports.render = (element, options) => (...args) => {
+  invariant(
+    isValidElement(element),
+    'render(): Received invalid React element'
+  );
+  invariant(
+    options === undefined || isPlainObject(options),
+    'render(): Received invalid options'
+  );
   const { render } = initialize({}, element, options);
-  if (!render) {
-    throw new Error("Can't use @untool/react mixin");
-  }
+  invariant(render, "Can't use @untool/react mixin");
   return render(...args);
 };
 
-exports.Miss = withRouter(function Miss({ staticContext }) {
+const Miss = ({ staticContext }) => {
   if (staticContext) {
     staticContext.miss = true;
   }
   return null;
-});
+};
+Miss.propTypes = {
+  staticContext: PropTypes.object,
+};
+exports.Miss = withRouter(Miss);
 
-exports.Status = withRouter(function Status({ staticContext, code }) {
+const Status = ({ staticContext, code }) => {
   if (staticContext) {
     staticContext.status = code;
   }
   return null;
-});
+};
+Status.propTypes = {
+  staticContext: PropTypes.object,
+  code: PropTypes.number.isRequired,
+};
+exports.Status = withRouter(Status);
 
-exports.Header = withRouter(function Header({ staticContext, name, value }) {
+const Header = ({ staticContext, name = '', value = '' }) => {
   if (staticContext) {
     staticContext.headers = { ...staticContext.headers, [name]: value };
   }
   return null;
-});
+};
+Header.propTypes = {
+  staticContext: PropTypes.object,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+};
+exports.Header = withRouter(Header);
 
 exports.importComponent = ({ load, moduleId }, name = 'default') => {
-  const Importer = withRouter(
-    class Importer extends Component {
-      constructor({ staticContext }) {
-        super();
-        if (staticContext) {
-          staticContext.modules.push(moduleId);
-        }
-        if (staticContext || __webpack_modules__[moduleId]) {
-          this.state = { Component: __webpack_require__(moduleId)[name] };
-        } else {
-          this.state = { loading: true };
-        }
+  class Importer extends Component {
+    constructor({ staticContext }) {
+      super();
+      if (staticContext) {
+        staticContext.modules.push(moduleId);
       }
-      componentDidMount() {
-        const { loader } = this.props;
-        const { loading } = this.state;
-        if (loading) {
-          const state = { Component: null, error: null, loading: false };
-          Promise.resolve()
-            .then(() => (loader ? loader(load) : load()))
-            .then(
-              ({ [name]: Component }) => this.setState({ ...state, Component }),
-              (error) => this.setState({ ...state, error })
-            );
-        }
-      }
-      render() {
-        const {
-          render = ({ Component, error, loading, ...props }) => {
-            return !(error || loading) ? createElement(Component, props) : null;
-          },
-          ownProps,
-        } = this.props;
-        return render({ ...ownProps, ...this.state });
+      if (staticContext || __webpack_modules__[moduleId]) {
+        this.state = { Component: __webpack_require__(moduleId)[name] };
+      } else {
+        this.state = { loading: true };
       }
     }
-  );
+    componentDidMount() {
+      const { loader } = this.props;
+      const { loading } = this.state;
+      if (loading) {
+        const state = { Component: null, error: null, loading: false };
+        Promise.resolve()
+          .then(() => (loader ? loader(load) : load()))
+          .then(
+            ({ [name]: Component }) => this.setState({ ...state, Component }),
+            (error) => this.setState({ ...state, error })
+          );
+      }
+    }
+    render() {
+      const {
+        render = ({ Component, error, loading, ...props }) => {
+          return !(error || loading) ? createElement(Component, props) : null;
+        },
+        ownProps,
+      } = this.props;
+      return render({ ...ownProps, ...this.state });
+    }
+  }
+  Importer.propTypes = {
+    staticContext: PropTypes.object,
+    ownProps: PropTypes.object.isRequired,
+    loader: PropTypes.func,
+    render: PropTypes.func,
+  };
+  const ImporterWithRouter = withRouter(Importer);
   return function Import({ loader, render, ...ownProps }) {
-    return createElement(Importer, { loader, render, ownProps });
+    return createElement(ImporterWithRouter, { loader, render, ownProps });
   };
 };
