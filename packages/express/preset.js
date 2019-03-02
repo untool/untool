73d1031a27e41d77303f96933df326d0da25337f
dmbch <daniel@dmbch.net>
2019-03-02 23:22:48 +0100

    feat(express): add config schema

--- packages/express/preset.js
+++ packages/express/preset.js
@@ -9,4 +9,22 @@ module.exports = {
   distDir: join('<rootDir>', 'dist'),
   gracePeriod: 30000,
   mixins: [join(__dirname, 'mixins')],
+  configSchema: {
+    https: {
+      oneOf: [
+        {
+          type: 'object',
+          properties: {
+            keyFile: { type: 'string', minLength: 1 },
+            certFile: { type: 'string', minLength: 1 },
+          },
+        },
+        { type: 'boolean' },
+      ],
+    },
+    host: { type: 'string' },
+    port: { oneOf: [{ type: 'string' }, { type: 'number' }] },
+    distDir: { type: 'string', minLength: 1 },
+    gracePeriod: { type: 'number' },
+  },
 };
