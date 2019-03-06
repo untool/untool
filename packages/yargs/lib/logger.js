575a6b5894008ccfdd7b3ced2dcb0f2c87bbdc5a
dmbch <daniel@dmbch.net>
2019-03-06 22:56:41 +0100

    feat(yargs): colorize log output

--- packages/yargs/lib/logger.js
+++ packages/yargs/lib/logger.js
@@ -1,9 +1,14 @@
 'use strict';
 /* eslint-disable no-console */
 
+const chalk = require('chalk');
 const escapeRegExp = require('escape-string-regexp');
 
-const logLevels = { error: 0, warn: 1, info: 2, request: 3 };
+const logLevels = { error: 0, warn: 1, info: 2, verbose: 3, request: 4 };
+
+const colorize = (string, color) => {
+  return chalk.enabled ? chalk[color](string) : `[${string}]`;
+};
 
 module.exports = exports = class Logger {
   constructor(name, workspace) {
@@ -23,26 +28,30 @@ module.exports = exports = class Logger {
   }
   error(error) {
     const { level, name } = this;
+    const prefix = colorize(`${name}:error`, 'red');
     if (level >= logLevels.error) {
-      console.error(`[${name}:error] ${this.renderError(error)}`);
+      console.error(`${prefix} ${this.renderError(error)}`);
     }
   }
   warn(warning) {
     const { level, name } = this;
+    const prefix = colorize(`${name}:warning`, 'yellow');
     if (level >= logLevels.warn) {
-      console.warn(`[${name}:warning] ${this.renderError(warning)}`);
+      console.warn(`${prefix} ${this.renderError(warning)}`);
     }
   }
   info(message) {
     const { level, name } = this;
+    const prefix = colorize(`${name}:info`, 'gray');
     if (level >= logLevels.info) {
-      console.log(`[${name}:info] ${message}`);
+      console.log(`${prefix} ${message}`);
     }
   }
   request(message) {
     const { level, name } = this;
+    const prefix = colorize(`${name}:request`, 'gray');
     if (level >= logLevels.request) {
-      console.log(`[${name}:request] ${message}`);
+      console.log(`${prefix} ${message}`);
     }
   }
 };
