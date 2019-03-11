30089a07f923fbb830df97c9f1df87a776188598
dmbch <daniel@dmbch.net>
2019-03-11 22:15:59 +0100

    refactor(yargs): simplify log mixin, introduce getLogger hook

--- packages/yargs/mixins/log/mixin.core.js
+++ packages/yargs/mixins/log/mixin.core.js
@@ -1,18 +1,26 @@
 'use strict';
 
-const prettyMS = require('pretty-ms');
+const {
+  sync: { override },
+} = require('mixinable');
 
-const { Mixin } = require('@untool/core');
+const {
+  Mixin,
+  internal: { validate, invariant },
+} = require('@untool/core');
 
 const Logger = require('../../lib/logger');
 
 const { logLevels } = Logger;
 
-module.exports = class CLIMixin extends Mixin {
+class LogMixin extends Mixin {
   constructor(...args) {
     super(...args);
     const { name, _workspace } = this.config;
     this.logger = new Logger(name, _workspace);
+  }
+  getLogger() {
+    return this.logger;
   }
   registerCommands(yargs) {
     yargs
@@ -30,36 +38,22 @@ module.exports = class CLIMixin extends Mixin {
   handleArguments(argv) {
     this.options = { ...this.options, ...argv };
     const { quiet, verbose } = this.options;
-    this.logger.setLogLevel(logLevels.info + verbose - quiet);
-    this.logger.info(
-      `started in ${process.env.NODE_ENV || 'development'} mode`
-    );
+    const logger = this.getLogger();
+    logger.setLogLevel(logLevels.info + verbose - quiet);
+    logger.info(`started in ${process.env.NODE_ENV || 'development'} mode`);
   }
   handleError(error) {
-    this.logger.error(error);
-  }
-  configureBuild(webpackConfig) {
-    const { LoggerPlugin } = require('../../lib/webpack');
-    webpackConfig.plugins.push(new LoggerPlugin(this.logger));
-  }
-  configureServer(app, middlewares, mode) {
-    if (mode !== 'static') {
-      const { loggerMiddleware } = require('../../lib/request');
-      app.use(loggerMiddleware(this.logger));
-    }
-  }
-  inspectServer(server) {
-    server.on('startup', (address) => {
-      const { basePath = '' } = this.config;
-      this.logger.info(`listening at ${address}/${basePath}`);
-    });
-    server.on('shutdown', () => {
-      const { gracePeriod } = this.config;
-      const timeout = prettyMS(gracePeriod);
-      this.logger.warn(`shutting down in ${timeout}`);
-    });
+    this.getLogger().error(error);
   }
   inspectWarnings(warnings) {
-    warnings.forEach((warning) => this.logger.warn(warning));
+    warnings.forEach((warning) => this.getLogger().warn(warning));
   }
+}
+
+LogMixin.strategies = {
+  getLogger: validate(override, ({ length }) => {
+    invariant(length === 0, 'getLogger(): Received unexpected argument(s)');
+  }),
 };
+
+module.exports = LogMixin;
