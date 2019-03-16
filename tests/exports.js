f5e9ce58583d558fd43d7e238590a7953755371c
dmbch <daniel@dmbch.net>
2019-03-16 22:26:42 +0100

    test: update tests for info package

--- tests/exports.js
+++ tests/exports.js
@@ -5,6 +5,7 @@ const { normalizeArgTypes } = require('./helpers/normalize');
 test('basic package exports', (t) => {
   t.snapshot(normalizeArgTypes(require('@untool/core')));
   t.snapshot(normalizeArgTypes(require('@untool/express')));
+  t.snapshot(normalizeArgTypes(require('@untool/info')));
   t.snapshot(normalizeArgTypes(require('@untool/react')));
   t.snapshot(normalizeArgTypes(require('@untool/webpack')));
   t.snapshot(normalizeArgTypes(require('@untool/yargs')));
