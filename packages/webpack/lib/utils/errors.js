8fda9c4b8a5e66100bc11f5e205346b0e2df6ffb
ZauberNerd <zaubernerd@zaubernerd.de>
2019-03-29 15:20:30 +0100

    refactor(webpack): remove obsolete BuildWarning

--- packages/webpack/lib/utils/errors.js
+++ packages/webpack/lib/utils/errors.js
@@ -10,13 +10,4 @@ class BuildError {
   }
 }
 
-class BuildWarning {
-  constructor(stack) {
-    this.name = this.constructor.name;
-    this.stack = `${this.name}: ${stack.replace(/\033?\[[0-9]{1,2}m/g, '')}`;
-    this.message = this.stack.slice(0, this.stack.indexOf(EOL));
-  }
-}
-
 exports.BuildError = BuildError;
-exports.BuildWarning = BuildWarning;
