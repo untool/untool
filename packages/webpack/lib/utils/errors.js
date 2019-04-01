26bf59c162ea530bdf87ad6421cbb9ea5a7ab7ce
dmbch <daniel@dmbch.net>
2019-04-01 15:05:25 +0200

    refactor(webpack): set up proper inheritance

--- packages/webpack/lib/utils/errors.js
+++ packages/webpack/lib/utils/errors.js
@@ -2,10 +2,11 @@
 
 const { EOL } = require('os');
 
-class BuildError {
+class BuildError extends Error {
   constructor(stack) {
+    super();
     this.name = this.constructor.name;
-    this.stack = `${this.name} in ${stack.replace(/\033?\[[0-9]{1,2}m/g, '')}`;
+    this.stack = `${this.name}: ${stack.replace(/\033?\[[0-9]{1,2}m/g, '')}`;
     this.message = this.stack.slice(0, this.stack.indexOf(EOL));
   }
 }
