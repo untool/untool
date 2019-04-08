0d59c1a4965dbfe0a7fcdea89fc21ac85842bd91
dmbch <daniel@dmbch.net>
2019-04-08 16:12:32 +0200

    refactor(webpack): use strip-ansi package instead of RegExp

--- packages/webpack/lib/utils/errors.js
+++ packages/webpack/lib/utils/errors.js
@@ -2,11 +2,13 @@
 
 const { EOL } = require('os');
 
+const stripAnsi = require('strip-ansi');
+
 class BuildError extends Error {
   constructor(stack) {
     super();
     this.name = this.constructor.name;
-    this.stack = `${this.name}: ${stack.replace(/\033?\[[0-9]{1,2}m/g, '')}`;
+    this.stack = `${this.name}: ${stripAnsi(stack)}`;
     this.message = this.stack.slice(0, this.stack.indexOf(EOL));
   }
 }
