77e22c039f9f8c5568360827c76d3847af912c73
Jonas Holland <mail@jhio.de>
2019-05-02 11:35:32 +0200

    feat(react): export renderToFragments function
    Co-authored-by: Emanuel Kluge <mail@emanuel-kluge.de>

--- packages/react/index.js
+++ packages/react/index.js
@@ -4,6 +4,7 @@ exports.internal = {
   babelPlugin: require('./lib/babel'),
   template: require('./lib/template'),
   runtime: require('./lib/runtime'),
+  renderToFragments: require('./lib/fragments'),
 };
 
 exports.configure = () => ({ ...exports });
