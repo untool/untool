622c5995011977cbf462eb83daeb7a758c026522
ZauberNerd <zaubernerd@zaubernerd.de>
2019-05-06 15:20:44 +0200

    refactor(webpack): apply review feedback

--- packages/webpack/lib/utils/errors.js
+++ packages/webpack/lib/utils/errors.js
@@ -3,6 +3,7 @@
 const { EOL } = require('os');
 
 const stripAnsi = require('strip-ansi');
+const serializeError = require('serialize-error');
 
 class BuildError extends Error {
   constructor(stack) {
@@ -11,6 +12,25 @@ class BuildError extends Error {
     this.stack = `${this.name}: ${stripAnsi(stack)}`;
     this.message = this.stack.slice(0, this.stack.indexOf(EOL));
   }
+  toJSON() {
+    return this.stack;
+  }
 }
 
 exports.BuildError = BuildError;
+
+class CompilerError extends Error {
+  constructor(error) {
+    super();
+    if (error && typeof error === 'object' && error.name && error.message) {
+      Object.assign(this, error);
+    } else {
+      this.message = error;
+    }
+  }
+  toJSON() {
+    return serializeError(this);
+  }
+}
+
+exports.CompilerError = CompilerError;
