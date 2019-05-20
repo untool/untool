22debb4c792599853d164d400c11addd3396d87d
ZauberNerd <zaubernerd@zaubernerd.de>
2019-05-20 15:13:27 +0200

    refactor(webpack): switch to event-source-polyfill for IE11
    As a preparation for the webpack 5 release, because webpack 5 doe not
    include the Node.js core module polyfills anymore and the `eventsource`
    packages has a lot of dependencies to Node core modules which results
    in quite a big browser bundle.

--- packages/webpack/lib/shims/develop.js
+++ packages/webpack/lib/shims/develop.js
@@ -2,7 +2,7 @@
 /* eslint-env browser */
 
 if (typeof window.EventSource === 'undefined') {
-  window.EventSource = require('eventsource');
+  window.EventSource = require('event-source-polyfill').EventSourcePolyfill;
 }
 
 require('webpack-hot-middleware/client');
